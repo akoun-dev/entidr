383a1a5f3280cdcc99cab0a81304b4d8cb226a31
ABOA AKOUN BERNARD <aboa.akoun40@gmail.com>
2025-08-14 20:52:58 +0000

    feat: add BPMN module specification

---
+++ addons/bpmn/index.ts
@@ -0,0 +1,16 @@
+// Exporter les routes (aucune pour le moment)
+export { default as routes } from './routes';
+
+// Exporter le manifeste
+export { default as manifest } from './manifest';
+
+// Aucun composant spécifique pour l'instant
+export const Components = {};
+
+export function initialize() {
+  // Code d'initialisation du module BPMN
+}
+
+export function cleanup() {
+  // Code de nettoyage du module BPMN
+}
